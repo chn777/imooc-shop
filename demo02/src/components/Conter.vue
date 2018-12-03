d9236843a23a55c0b44e4860530a65069441a088
JiangJun <jiangjun@JiangJuns-MacBook-Pro.local>
2018-12-03 21:42:50 +0800

    demo1 是一webpack-simple的demo demo002是一个webpack的demo

---
+++ demo02/src/components/Conter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <button @click="handleAddClick"> + </button>
+      <br/>
+      <span>kid:{{num}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Conter',
+  props: ['num'],
+  methods: {
+    handleAddClick () {
+      this.$emit('add', this.num)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ demo02/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <span>father:{{num}}</span>
+    <Conter :num="num" v-on:add="addFromSub"></Conter>
+  </div>
+</template>
+
+<script>
+import Conter from './Conter'
+export default {
+  name: 'HelloWorld',
+  components: {Conter},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      num: 10
+    }
+  },
+  methods: {
+    addFromSub (val) {
+      this.num = val + 1
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
